dbfc4d1ea48f41b174cb764a1f95d068e3b120f2
VsCode <vscode@drukdim.com>
2026-03-07 15:55:12 +0200

    Refactor bridge protocol structs into header

---
+++ firmware/bridge/src/bridge_proto.h
@@ -0,0 +1,64 @@
+#pragma once
+#include <stdint.h>
+
+enum UsbType : uint8_t {
+  USB_SET_FLOW   = 1,
+  USB_PING       = 2,
+  USB_SET_MAXLPM = 3
+};
+
+enum UsbResp : uint8_t {
+  USB_BRIDGE_STATUS = 101
+};
+
+enum NowType : uint8_t {
+  NOW_CMD_FLOW   = 1,
+  NOW_SET_MAXLPM = 2,
+  NOW_ACK        = 100,
+  NOW_STATUS     = 101
+};
+
+#pragma pack(push, 1)
+
+struct UsbHdr {
+  uint8_t proto;
+  uint8_t type;
+  uint16_t seq;
+  uint32_t mono_ms;
+};
+
+struct NowHdr {
+  uint8_t proto;
+  uint8_t type;
+  uint16_t seq;
+};
+
+struct NowCmdFlow {
+  NowHdr  h;
+  int32_t target_milli_lpm;
+  uint8_t flags;
+  uint16_t crc;
+};
+
+struct NowSetMaxLpm {
+  NowHdr  h;
+  int32_t pump_max_milli_lpm;
+  uint16_t crc;
+};
+
+struct NowAck {
+  NowHdr  h;
+  uint8_t applied_code;
+  uint16_t err_flags;
+  uint16_t crc;
+};
+
+struct NowStatus {
+  NowHdr  h;
+  uint8_t applied_code;
+  uint16_t err_flags;
+  uint32_t uptime_ms;
+  uint16_t crc;
+};
+
+#pragma pack(pop)
--- firmware/bridge/src/main.cpp
+++ firmware/bridge/src/main.cpp
@@ -3,6 +3,7 @@
 #include <esp_now.h>
 #include <esp_wifi.h>
 #include "cobs_crc.h"
+#include "bridge_proto.h"
 
 // -------------------- Config --------------------
 static const uint8_t PROTO = 1;
@@ -23,58 +24,6 @@ static const uint32_t PUMP_OFFLINE_MS = 1200;
 
 // Pump MAC (your Pump-ESP): D4:E9:F4:FA:88:34
 static uint8_t PUMP_MAC[6] = {0xD4, 0xE9, 0xF4, 0xFA, 0x88, 0x34};
-
-// -------------------- Protocol --------------------
-enum UsbType : uint8_t {
-  USB_SET_FLOW   = 1, // body: i32 milli_lpm, u8 flags
-  USB_PING       = 2, // body: none
-  USB_SET_MAXLPM = 3  // body: i32 pump_max_milli_lpm
-};
-
-enum UsbResp : uint8_t {
-  USB_BRIDGE_STATUS = 101
-};
-
-enum NowType : uint8_t {
-  NOW_CMD_FLOW   = 1,
-  NOW_SET_MAXLPM = 2,
-  NOW_ACK        = 100,
-  NOW_STATUS     = 101
-};
-
-#pragma pack(push, 1)
-struct UsbHdr { uint8_t proto; uint8_t type; uint16_t seq; uint32_t mono_ms; };
-
-struct NowHdr { uint8_t proto; uint8_t type; uint16_t seq; };
-
-struct NowCmdFlow {
-  NowHdr  h;
-  int32_t target_milli_lpm;
-  uint8_t flags; // bit1 STOP priority
-  uint16_t crc;
-};
-
-struct NowSetMaxLpm {
-  NowHdr  h;
-  int32_t pump_max_milli_lpm;
-  uint16_t crc;
-};
-
-struct NowAck {
-  NowHdr  h; // seq=acked seq
-  uint8_t applied_code;
-  uint16_t err_flags;
-  uint16_t crc;
-};
-
-struct NowStatus {
-  NowHdr  h; // seq=last_cmd_seq
-  uint8_t applied_code;
-  uint16_t err_flags;
-  uint32_t uptime_ms;
-  uint16_t crc;
-};
-#pragma pack(pop)
 
 // -------------------- State --------------------
 static uint8_t  g_last_applied = 0;
