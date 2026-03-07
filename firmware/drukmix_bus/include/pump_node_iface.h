aeef74ed9df0f77ef1ea24ca300d32c9d2a73073
VsCode <vscode@drukdim.com>
2026-03-07 16:30:17 +0200

    Add shared pump node interface

---
+++ firmware/drukmix_bus/include/pump_node_iface.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <stdint.h>
+
+struct PumpNodeStatus {
+  bool online = false;
+  bool running = false;
+  bool faulted = false;
+  uint16_t fault_code = 0;
+
+  int32_t target_milli_lpm = 0;
+  int32_t actual_milli_lpm = 0;
+  int32_t max_milli_lpm = 0;
+
+  int32_t hw_setpoint_raw = 0;
+  int16_t actual_speed_raw = 0;
+};
+
+class PumpNodeIface {
+public:
+  virtual ~PumpNodeIface() = default;
+
+  virtual void begin() = 0;
+  virtual void update() = 0;
+
+  virtual bool set_flow(int32_t target_milli_lpm) = 0;
+  virtual bool stop() = 0;
+  virtual bool reset_fault() = 0;
+  virtual bool get_status(PumpNodeStatus* st) = 0;
+};
